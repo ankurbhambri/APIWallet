f0082701c5b6074447bb9eeedcf61d095305306e
Ankur Bhambri <ankur.bhambri@renewbuy.com>
2020-08-30 23:38:19 +0530

    chnages

--- api_wallet/settings.py
+++ api_wallet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c)mbo6_j0&s6)b7x46z!x(+n)3to(kz3u)p+vbdwp3%i5jmo@s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['intense-castle-81068.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['intense-castle-81068.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -51,6 +51,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,4 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
